4dc72463c531c9a9b6d632c4dd3def5fd856a18e
Arfifa Rahman <arfifarahman509@gmail.com>
2020-05-04 20:50:45 +0700

    add crud donationHistory

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,13 @@ const userRoute = require('./user');
 const projectRouter = require('./project');
 const sliderRoute = require('./slider');
 const walletRoute = require('./wallet');
+const donationHistoryRoute = require('./donationHistory');
 
 Route.use('/user', userRoute)
   .use('/project', projectRouter)
   .use('/uploads', express.static('./uploads'))
   .use('/slider', sliderRoute)
-  .use('/wallet', walletRoute);
+  .use('/wallet', walletRoute)
+  .use('/donationHistory', donationHistoryRoute);
 
 module.exports = Route;
